b96f6e0bbcefd19e65184c10d4801308d3ae4689
EzeGramajo <90995215+EzeGramajo@users.noreply.github.com>
2022-11-04 17:57:59 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,31 @@
 def principal():
-  pass
-
+  n = -1
+  while n != 8:
+    op = int(input('Ingrese una opcion: '))
+    if op == 1:
+      pass
+    
+    elif op == 2:
+      pass
+    
+    elif op == 3:
+      pass
+    
+    elif op == 4:
+      pass
+    
+    elif op == 5:
+      pass
+    
+    elif op == 6:
+      pass
+    
+    elif op == 7:
+      pass
+    
+    else:
+      print('Adios.')
+      
 if __name__ == '__main__':
   principal()
   
